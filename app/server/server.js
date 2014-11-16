ba7567a93cabc4715d6b9c1363d380d6b3e9234d
Emmanuel Pilande <EmmanuelPilande@erp.local.tld>
2014-11-16 13:57:55 -1000

    Create feed content response method to update alerts collection

--- app/server/server.js
+++ app/server/server.js
@@ -1,7 +1,7 @@
 Meteor.methods({
-  updateCount: function(count) {
-    AlertsCollection.insert({
-      'clicked' : count
-    });
+  feedContentResponse: function(feedId, reponse) {
+    console.log(feedId, reponse);
+    AlertsCollection.update({_id: feedId}, {$set:{"status": reponse}});
+    console.log("Done");
   }
 });

363ca348abb7cf1bb3f94dd12793031e8b05958e
Emmanuel Pilande <EmmanuelPilande@erp.local.tld>
2014-11-16 13:58:09 -1000

    Allow users to update alerts collection

--- app/server/models/alerts.js
+++ app/server/models/alerts.js
@@ -3,3 +3,10 @@ AlertsCollection = new Meteor.Collection("alerts");
 Meteor.publish("alerts", function(){
   return AlertsCollection.find();
 });
+
+
+AlertsCollection.allow({
+  update: function(userId, doc, fieldNames, modifier) {
+    return true;
+  }
+});

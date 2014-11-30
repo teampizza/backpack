67063ab9fe16c376fd6ea0a522f67cfacd7ecbc8
Trevor Alexander <talex@privatdemail.net>
2014-11-29 16:44:20 -1000

    Added DB interaction code to pcap parser

--- backend/parser/pcap_parser.js
+++ backend/parser/pcap_parser.js
@@ -1,3 +1,6 @@
+var mongojs = require('mongojs');
+var db = mongojs.connect('localhost:27017/backpack', ['alerts']);
+
 var pcap = require('pcap');
 // environment var name
 var ifacevar = 'NET_INTERFACE';
@@ -18,16 +21,24 @@ sourcematcher = /connect.facebook.net/;
 referermatcher = /Referer: (.*)/;
 // console.log("Listening on " + pcap_session.device_name);
 
+// what we're looking for now is the site that referred us to FB
+var referer;
 pcap_session.on('packet', function (raw_packet) {
     var packet = pcap.decode.packet(raw_packet),
         data = packet.link.ip.tcp.data;
     
     if (data && sourcematcher.test(data.toString())) {
-        // console.log(pcap.print.packet(packet));
-        console.log(data.toString());
+        console.log(pcap.print.packet(packet));
+        // console.log(data.toString());
 				
 				if (referermatcher.test(data.toString())) {
-						console.log(referermatcher.exec(data.toString())[1]);
+						referer = referermatcher.exec(data.toString())[1]
+						console.log(referer);
+						
+						// save to DB
+						db.alerts.save({msg: referer,
+														created_date: Date.now()
+														});
 				}
     }
 });

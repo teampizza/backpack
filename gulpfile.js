e6188d597d8936aa05448c763233c1f55311468a
Emmanuel Pilande <EmmanuelPilande@ERP.local>
2014-11-30 03:46:43 -1000

    Add comments to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,26 @@ var gulp = require('gulp'),
     prefix = require('gulp-autoprefixer'),
     notify = require('gulp-notify');
 
+// Growl error messages
 function onError(err) {
   notify.onError(err.message)(err); // for growl
   this.emit('end');
 }
 
+// Compile sass to css and auto prefix
 gulp.task('sass', function(){
   return gulp.src('app/client/styles/styles.scss')
-    .pipe(plumber(onError))
-    .pipe(sass())
-    .pipe(prefix())
+    .pipe(plumber(onError)) // error handler
+    .pipe(sass()) // compile
+    .pipe(prefix()) // autoprefix
     .pipe(gulp.dest('app/client/styles'));
 });
 
+// Watch files for changes
 gulp.task('watch', function() {
+  // If *.scss file change call 'sass' task
   gulp.watch('app/client/styles/**/*.scss', ['sass']);
 });
 
+// Default task
 gulp.task('default', ['watch']);

8aceff1fbfef45952fe43b488bf97b5c694d51aa
Trevor Alexander <talex@privatdemail.net>
2014-12-07 12:47:28 -1000

    parser now writes bulk packet to db.netdata

--- backend/parser/pcap_parser.js
+++ backend/parser/pcap_parser.js
@@ -1,32 +1,43 @@
 var mongojs = require('mongojs');
+var merge = require('deepmerge');
+
 var db = mongojs.connect('localhost:27017/backpack', ['netdata']);
 
 var pcap = require('pcap');
 // environment var name
-// var iface = "wlan0";
 var ifacevar = 'NET_INTERFACE';
 var iface = process.env[ifacevar];
 
-// filter syntax comes from 'man pcap-filter'
 
+// filter syntax comes from 'man pcap-filter'
 // for now let's do TCP/HTTP
 pcap_session = pcap.createSession(iface, "ip proto \\tcp");
 tcp_tracker = new pcap.TCP_tracker();
 
-
-referermatcher = /Referer: (.*)/;
 // TODO get this into a field-separated JSON
 // issue -- lots of data that can't be decoded (https, etc.)
 // proposed solution:
 // store available fields as able, then rely on models to toString() packet data
 pcap_session.on('packet', function (raw_packet) {
     var packet = pcap.decode.packet(raw_packet);
-		var data = packet.link.ip.tcp.data;
+		// var data = packet.link.ip.tcp.data;
 		// if (data && referermatcher.test(data.toString())) {
 		// 		console.log(data.toString());
 		// 		// console.log(pcap.print.packet(packet));
 		// }
 		// console.log(JSON.stringify(packet));
+		
+		// send to netdata DB
+		db.netdata.save(build_payload(packet,'node_pcap'));
 });
 
-// TODO put it into netdata
+
+function build_payload(dataobj,backend_name) {
+		backend_name = backend_name || 'node_pcap'; // default arg
+		// TODO test to make sure obj is object
+
+		// build object to be stored 
+		var payload = {};
+		payload.backend = backend_name;
+		return(merge(payload,dataobj));
+}

9d4e69de5e6192a142b87dc7b85145b19844ba41
Trevor Alexander <talex@privatdemail.net>
2014-12-07 15:57:08 -1000

    Bugfixes to parsing start functions
    - pcap session starter now escapes proto array properly
    - db capper parses args properly for mongojs .isCapped()

--- backend/parser/pcap_parser.js
+++ backend/parser/pcap_parser.js
@@ -9,11 +9,16 @@ var ifacevar = 'NET_INTERFACE';
 var iface = process.env[ifacevar];
 
 
-// filter syntax comes from 'man pcap-filter'
-// for now let's do TCP/HTTP
-pcap_session = pcap.createSession(iface, "ip proto \\tcp");
+// make sure netdata DB is capped (10Mb here)
+// TODO take user options for this
+cap_dbcollection(db,'netdata',10000000);
 
+// for now let's do TCP
+// TODO take model spec for this
+pcap_session = start_capsession(iface, ['tcp']);
 
+// session-based tracking is a higher level
+// TODO revisit this
 // tcp_tracker = new pcap.TCP_tracker();
 
 // issue -- lots of data that can't be decoded (https, etc.)
@@ -21,12 +26,8 @@ pcap_session = pcap.createSession(iface, "ip proto \\tcp");
 // store available fields as able, then rely on models to toString() packet data
 pcap_session.on('packet', function (raw_packet) {
     var packet = pcap.decode.packet(raw_packet);
-		// var data = packet.link.ip.tcp.data;
-		// if (data && referermatcher.test(data.toString())) {
-		// 		console.log(data.toString());
-		// 		// console.log(pcap.print.packet(packet));
-		// }
-		// console.log(JSON.stringify(packet));
+
+		// console.log(packet);
 		
 		// send to netdata DB
 		db.netdata.save(build_payload(packet,'node_pcap'));
@@ -34,8 +35,15 @@ pcap_session.on('packet', function (raw_packet) {
 
 function start_capsession(net_iface, protocols) {
 		// TODO test inputs for validity or catch errors
+		// filter syntax comes from 'man pcap-filter'
+
+		// escape protos
+		var esc_protocols = protocols.map(function(str) {
+				return("\\"+str);
+		});
+		var protostring = 'ip proto (' + esc_protocols.join(' or ') + ')';
 		return(
-				pcap.createSession(net_iface,protocols.join(" or "))
+				pcap.createSession(net_iface,protostring)
 		);
 }
 
@@ -51,9 +59,12 @@ function build_payload(dataobj,backend_name) {
 
 function cap_dbcollection(thisdb, collection, dbsize) {
 		// util function to check if DB is capped and if not, cap it to user-set
-		// size
-		// TODO test
-		if(! thisdb.collection.isCapped()) {
+		// size 
+    // warning: blocks db write globally while working, should only be run at
+		// start
+
+		// TODO tests
+		if(! thisdb[collection].isCapped(function(err,res) {return(res)})) {
 				thisdb.runCommand({"convertToCapped": collection, size: dbsize});
 		}
 }

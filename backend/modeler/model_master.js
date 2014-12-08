f85ba2eefe7557728347e351c86b344977955a17
Trevor Alexander <talex@privatdemail.net>
2014-12-07 16:33:46 -1000

    Models now all auto-included into model_master.js
    Moved models into backend subdirectory also.

--- backend/modeler/model_master.js
+++ backend/modeler/model_master.js
@@ -2,6 +2,15 @@ var mongojs = require('mongojs');
 var mubsub = require('mubsub');
 var netdata_client = mubsub('mongodb://localhost:27017/backpack');
 var alertdb = mongojs.connect('localhost:27017/backpack', ['alerts']);
+// TODO import all models through manifest
+// TODO straighten this path tripe out
+// var model_socialbeacon = require('./backend/modeler/models/socialbeacon/model.js');
+var models = require('include-all')({
+  dirname     :  process.cwd() + '/backend/modeler/models',
+  filter      :  /(.+)\.js$/,
+  excludeDirs :  /^\.(git|svn)$/,
+  optional    :  true
+});
 
 // original POC, search for Facebook Connect social beacon
 // liberally borrowed from
@@ -14,40 +23,10 @@ var alertdb = mongojs.connect('localhost:27017/backpack', ['alerts']);
 
 var netdata_channel = netdata_client.channel('netdata');
 console.log(netdata_channel);
+
 // when a new document is added to netdata, pass it to models
 // TODO unify into single model_dispatch function
 var netdata_sub = netdata_channel.subscribe(function(newdata) {
 		console.log(newdata.toString());
-		model_socialbeacon(newdata);
+		models.socialbeacon.model(newdata);
 });
-
-
-// TODO move this out to model dir
-function model_socialbeacon(netdata) {
-		// TODO test input, fail gracefully
-		// e.g. for protocol, etc.
-		var testdata = netdata.link.ip.tcp.data;
-
-		// things we want to match
-		sourcematcher = /connect.facebook.net/;
-		referermatcher = /Referer: (.*)/;
-		// output-use vars
-		var referer;
-
-		// do the matching
-		// TODO refactor, tests can be combined
-		if (testdata && sourcematcher.test(testdata.toString())) {
-				console.log(testdata.toString());
-				
-				if (referermatcher.test(testdata.toString())) {
-						referer = referermatcher.exec(testdata.toString())[1];
-						console.log(referer);
-						
-						// save to alerts DB
-						// TODO break this out into another function
-						alertdb.alerts.save({msg: referer,
-																 created_date: Date.now()
-																});
-				}
-		}
-}
---
+++ backend/modeler/models/socialbeacon/model.js
@@ -0,0 +1,30 @@
+var socialbeacon = function(netdata) {
+		// TODO test input, fail gracefully
+		// e.g. for protocol, etc.
+		var testdata = netdata.link.ip.tcp.data;
+
+		// things we want to match
+		sourcematcher = /connect.facebook.net/;
+		referermatcher = /Referer: (.*)/;
+		// output-use vars
+		var referer;
+
+		// do the matching
+		// TODO refactor, tests can be combined
+		if (testdata && sourcematcher.test(testdata.toString())) {
+				console.log(testdata.toString());
+				
+				if (referermatcher.test(testdata.toString())) {
+						referer = referermatcher.exec(testdata.toString())[1];
+						console.log(referer);
+						
+						// save to alerts DB
+						// TODO break this out into unified alert function
+						alertdb.alerts.save({msg: referer,
+																 created_date: Date.now()
+																});
+				}
+		}
+}
+
+module.exports = socialbeacon;

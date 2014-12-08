fe12605a4d3c3595b20ebef9806f223e796a0a06
Trevor Alexander <talex@privatdemail.net>
2014-12-07 14:41:50 -1000

    modeler with mubsub subscription to netdata DB
    Doesn't work yet, but is close--why not?

--- backend/modeler/model_master.js
+++ backend/modeler/model_master.js
@@ -1,5 +1,6 @@
 var mongojs = require('mongojs');
-var netdatadb = mongojs.connect('localhost:27017/backpack', ['netdata']);
+var mubsub = require('mubsub');
+var netdata_client = mubsub('mongodb://localhost:27017/backpack');
 var alertdb = mongojs.connect('localhost:27017/backpack', ['alerts']);
 
 // original POC, search for Facebook Connect social beacon
@@ -8,27 +9,45 @@ var alertdb = mongojs.connect('localhost:27017/backpack', ['alerts']);
 
 // TODO poll netdata DB
 
-sourcematcher = /connect.facebook.net/;
-referermatcher = /Referer: (.*)/;
-
-
-// TODO replace with queries to netdata
 // TODO break this out to a sub-model process
 // what we're looking for now is the site that referred us to FB
-var referer;
 
-if (data && sourcematcher.test(data.toString())) {
-    console.log(pcap.print.packet(packet));
-    // console.log(data.toString());
-		
-		if (referermatcher.test(data.toString())) {
-				referer = referermatcher.exec(data.toString())[1];
-				console.log(referer);
+var netdata_channel = netdata_client.channel('netdata');
+console.log(netdata_channel);
+// when a new document is added to netdata, pass it to models
+// TODO unify into single model_dispatch function
+var netdata_sub = netdata_channel.subscribe(function(newdata) {
+		console.log(newdata.toString());
+		model_socialbeacon(newdata);
+});
+
+
+// TODO move this out to model dir
+function model_socialbeacon(netdata) {
+		// TODO test input, fail gracefully
+		// e.g. for protocol, etc.
+		var testdata = netdata.link.ip.tcp.data;
+
+		// things we want to match
+		sourcematcher = /connect.facebook.net/;
+		referermatcher = /Referer: (.*)/;
+		// output-use vars
+		var referer;
+
+		// do the matching
+		// TODO refactor, tests can be combined
+		if (testdata && sourcematcher.test(testdata.toString())) {
+				console.log(testdata.toString());
 				
-				// save to alerts DB
-				// TODO no changes here
-				alertdb.alerts.save({msg: referer,
-														 created_date: Date.now()
-														});
+				if (referermatcher.test(testdata.toString())) {
+						referer = referermatcher.exec(testdata.toString())[1];
+						console.log(referer);
+						
+						// save to alerts DB
+						// TODO break this out into another function
+						alertdb.alerts.save({msg: referer,
+																 created_date: Date.now()
+																});
+				}
 		}
 }

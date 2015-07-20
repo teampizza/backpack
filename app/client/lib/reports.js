4aefb4cc0c34b2ce679e035b37e7a3ac6724a793
Emmanuel Pilande <manny@sudokrew.com>
2015-07-20 11:36:23 -1000

    Display reports for new, read, & ignored today

--- app/client/lib/reports.js
+++ app/client/lib/reports.js
@@ -1,6 +1,31 @@
+'use strict';
+
+var dateToday = moment().valueOf();
+var oneDayAgo = moment().subtract(1,'days').valueOf();
+
 Template.reports.helpers({
   alerts: function () {
-    return alertsCollection.find({status: 'new'}, {sort: {created: -1}});
+    return alertsCollection.find();
+  },
+  today: function() {
+    var alerts = alertsCollection.find({created: {$gte: oneDayAgo, $lt: dateToday}}).fetch();
+    var newAlerts = _.filter(alerts, function(alert) {
+          return alert.status === 'new';
+        }).length;
+
+    var readAlerts = _.filter(alerts, function(alert) {
+          return alert.status === 'read';
+        }).length;
+
+    var ignoredAlerts = _.filter(alerts, function(alert) {
+          return alert.status === 'ignored';
+        }).length;
+
+    return {
+      'todayNewCount': newAlerts,
+      'todayReadCount': readAlerts,
+      'todayIgnoredCount': ignoredAlerts
+    };
   }
 });
 

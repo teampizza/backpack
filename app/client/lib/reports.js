43d719d8c3d7d0358f9afc24e3d32e67568d0ef3
Emmanuel Pilande <manny@sudokrew.com>
2015-07-20 11:43:08 -1000

    Display alert reports for this week & month

--- app/client/lib/reports.js
+++ app/client/lib/reports.js
@@ -2,6 +2,8 @@
 
 var dateToday = moment().valueOf();
 var oneDayAgo = moment().subtract(1,'days').valueOf();
+var oneWeekAgo = moment().subtract(1,'weeks').valueOf();
+var oneMonthAgo = moment().subtract(1,'months').valueOf();
 
 Template.reports.helpers({
   alerts: function () {
@@ -22,9 +24,49 @@ Template.reports.helpers({
         }).length;
 
     return {
-      'todayNewCount': newAlerts,
-      'todayReadCount': readAlerts,
-      'todayIgnoredCount': ignoredAlerts
+      'newCount': newAlerts,
+      'readCount': readAlerts,
+      'ignoredCount': ignoredAlerts
+    };
+  },
+  thisWeek: function() {
+    var alerts = alertsCollection.find({created: {$gte: oneWeekAgo, $lt: dateToday}}).fetch();
+    var newAlerts = _.filter(alerts, function(alert) {
+          return alert.status === 'new';
+        }).length;
+
+    var readAlerts = _.filter(alerts, function(alert) {
+          return alert.status === 'read';
+        }).length;
+
+    var ignoredAlerts = _.filter(alerts, function(alert) {
+          return alert.status === 'ignored';
+        }).length;
+
+    return {
+      'newCount': newAlerts,
+      'readCount': readAlerts,
+      'ignoredCount': ignoredAlerts
+    };
+  },
+  thisMonth: function() {
+    var alerts = alertsCollection.find({created: {$gte: oneMonthAgo, $lt: dateToday}}).fetch();
+    var newAlerts = _.filter(alerts, function(alert) {
+          return alert.status === 'new';
+        }).length;
+
+    var readAlerts = _.filter(alerts, function(alert) {
+          return alert.status === 'read';
+        }).length;
+
+    var ignoredAlerts = _.filter(alerts, function(alert) {
+          return alert.status === 'ignored';
+        }).length;
+
+    return {
+      'newCount': newAlerts,
+      'readCount': readAlerts,
+      'ignoredCount': ignoredAlerts
     };
   }
 });

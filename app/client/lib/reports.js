dc37bf35471fd2cffd698a2ac3b89af7ffb44a24
Emmanuel Pilande <manny@sudokrew.com>
2015-07-21 14:09:17 -1000

    Refactor alert report helpers

--- app/client/lib/reports.js
+++ app/client/lib/reports.js
@@ -10,64 +10,34 @@ Template.reports.helpers({
     return alertsCollection.find();
   },
   today: function() {
-    var alerts = alertsCollection.find({created: {$gte: oneDayAgo, $lt: dateToday}}).fetch();
-    var newAlerts = _.filter(alerts, function(alert) {
-          return alert.status === 'new';
-        }).length;
-
-    var readAlerts = _.filter(alerts, function(alert) {
-          return alert.status === 'read';
-        }).length;
-
-    var ignoredAlerts = _.filter(alerts, function(alert) {
-          return alert.status === 'ignored';
-        }).length;
-
-    return {
-      'newCount': newAlerts,
-      'readCount': readAlerts,
-      'ignoredCount': ignoredAlerts
-    };
+    return alertReports(oneDayAgo);
   },
   thisWeek: function() {
-    var alerts = alertsCollection.find({created: {$gte: oneWeekAgo, $lt: dateToday}}).fetch();
-    var newAlerts = _.filter(alerts, function(alert) {
-          return alert.status === 'new';
-        }).length;
-
-    var readAlerts = _.filter(alerts, function(alert) {
-          return alert.status === 'read';
-        }).length;
-
-    var ignoredAlerts = _.filter(alerts, function(alert) {
-          return alert.status === 'ignored';
-        }).length;
-
-    return {
-      'newCount': newAlerts,
-      'readCount': readAlerts,
-      'ignoredCount': ignoredAlerts
-    };
+    return alertReports(oneWeekAgo);
   },
   thisMonth: function() {
-    var alerts = alertsCollection.find({created: {$gte: oneMonthAgo, $lt: dateToday}}).fetch();
-    var newAlerts = _.filter(alerts, function(alert) {
-          return alert.status === 'new';
-        }).length;
-
-    var readAlerts = _.filter(alerts, function(alert) {
-          return alert.status === 'read';
-        }).length;
-
-    var ignoredAlerts = _.filter(alerts, function(alert) {
-          return alert.status === 'ignored';
-        }).length;
-
-    return {
-      'newCount': newAlerts,
-      'readCount': readAlerts,
-      'ignoredCount': ignoredAlerts
-    };
+    return alertReports(oneMonthAgo);
   }
 });
 
+function alertReports(dateAgo) {
+  var alerts = alertsCollection.find({created: {$gte: dateAgo, $lt: dateToday}}).fetch();
+
+  var newAlerts = _.filter(alerts, function(alert) {
+        return alert.status === 'new';
+      }).length;
+
+  var readAlerts = _.filter(alerts, function(alert) {
+        return alert.status === 'read';
+      }).length;
+
+  var ignoredAlerts = _.filter(alerts, function(alert) {
+        return alert.status === 'ignored';
+      }).length;
+
+  return {
+    'newCount': newAlerts,
+    'readCount': readAlerts,
+    'ignoredCount': ignoredAlerts
+  };
+}

b050ac77ccdd97a40a6ad9c582a0fdc29d7d2380
Trevor Alexander <talex@privatdemail.net>
2015-09-26 23:03:44 +0200

    refactor of model_master pt. 1: real functions

--- backend/modeler/model_master.js
+++ backend/modeler/model_master.js
@@ -3,33 +3,47 @@
 require('dotenv').load();
 var mongojs = require('mongojs');
 var mubsub = require('mubsub');
-var netdata = mubsub(process.env.MONGO_URL);
-var alertdb = mongojs.connect(process.env.MONGO_URL.slice(10), ['alerts']);
-// TODO import all models through manifest
-// TODO straighten this path tripe out
-require('./models/socialbeacon/model.js');
-var models = require('include-all')({
-  dirname     :  process.cwd() + '/backend/modeler/models',
-  filter      :  /(.+)\.js$/,
-  excludeDirs :  /^\.(git|svn)$/,
-  optional    :  true
-});
+var defaultNetdata = mubsub(process.env.MONGO_URL);
+// TODO document or fix this fucking ass-schema
+var defaultAlertDB = [process.env.MONGO_URL.slice(10), ['alerts']];
+var defaultModelDir = process.cwd() + '/backend/modeler/models';
 
-var netdataChannel = netdata.channel('netdata');
-// are we up? print
-// console.log(netdataChannel);
+// bring up modeler components
+// returns subscription to alerts with insert
+function initModeler(dataURL, alertURL, modelDir) {
+	// defaults
+	modelDir = (modelDir !== undefined ? modelDir : defaultModelDir);
+	alertURL = (alertURL !== undefined ? alertURL : defaultAlertDB);
+	dataURL = (dataURL !== undefined ? dataURL : defaultNetdata);
 
-// when a new document is added to netdata, pass it to models
-// TODO unify into single model_dispatch function
-netdataChannel.subscribe('newpacket', function(newdata) {
+	var alertDB = mongojs.connect(defaultAlertDB[0], defaultAlertDB[1]);
+	var netData = mubsub(dataURL);
 	
-	// TODO collect model alerts by iterating through models object asyncly
-	var modelalerts = [models.socialbeacon.model(newdata)];
-	// save nonempty elements to alerts DB
-	// http://stackoverflow.com/a/5443800/2023432
-	insertAlerts(modelalerts.filter(Boolean), alertdb);
+	// TODO import all models through manifest
+	// TODO straighten this path tripe out
+	// require('./models/socialbeacon/model.js');
+	var models = require('include-all')({
+		dirname			:	 modelDir,
+		filter			:	 /(.+)\.js$/,
+		excludeDirs :	 /^\.(git|svn)$/,
+		optional		:	 true
+	});
 
-});
+	var netDataChannel = netData.channel('netdata');
+	// are we up? print
+	// console.log(netDataChannel);
+
+	// when a new document is added to netdata, pass it to models
+	// TODO unify into single model_dispatch function
+	return netDataChannel.subscribe('newpacket', function(newData) {
+		
+		// TODO collect model alerts by iterating through models object asyncly
+		var modelAlerts = [models.socialbeacon.model(newData)];
+		// save nonempty elements to alerts DB
+		// http://stackoverflow.com/a/5443800/2023432
+		insertAlerts(modelAlerts.filter(Boolean), alertDB);
+	});
+}
 
 function insertAlerts(objarray, db) {
 	// TODO insert response callback
@@ -39,3 +53,7 @@ function insertAlerts(objarray, db) {
 		});
 	}
 }
+
+module.exports = {
+  initModeler : initModeler
+};

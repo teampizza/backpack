710652b4616083fa08f975a56219cfa0eede841f
Trevor Alexander <talex@privatdemail.net>
2015-09-29 00:50:10 +0200

    cleared up model_master behavior

--- backend/modeler/model_master.js
+++ backend/modeler/model_master.js
@@ -1,27 +1,27 @@
 'use strict';
-// load env vars
-require('dotenv').load();
 var mongojs = require('mongojs');
 var mubsub = require('mubsub');
-var defaultNetdata = mubsub(process.env.MONGO_URL);
-// TODO document or fix this fucking ass-schema
-var defaultAlertDB = [process.env.MONGO_URL.slice(10), ['alerts']];
-var defaultModelDir = process.cwd() + '/backend/modeler/models';
-
+var async = require('async');
+// load env vars
+require('dotenv').load();
 // bring up modeler components
 // returns subscription to alerts with insert
 function initModeler(dataURL, alertURL, modelDir) {
 	// defaults
+  var defaultDataURL = process.env.MONGO_URL;
+  // TODO document or fix this fucking ass-schema
+  var defaultAlertDB = [process.env.MONGO_URL.slice(10), ['alerts']];
+  var defaultModelDir = process.cwd() + '/backend/modeler/models/';
+
 	modelDir = (modelDir !== undefined ? modelDir : defaultModelDir);
 	alertURL = (alertURL !== undefined ? alertURL : defaultAlertDB);
-	dataURL = (dataURL !== undefined ? dataURL : defaultNetdata);
+	dataURL = (dataURL !== undefined ? dataURL : defaultDataURL);
 
-	var alertDB = mongojs.connect(defaultAlertDB[0], defaultAlertDB[1]);
+	var alertDB = mongojs.connect(defaultAlertDB);
+
 	var netData = mubsub(dataURL);
 	
-	// TODO import all models through manifest
 	// TODO straighten this path tripe out
-	// require('./models/socialbeacon/model.js');
 	var models = require('include-all')({
 		dirname			:	 modelDir,
 		filter			:	 /(.+)\.js$/,
@@ -35,14 +35,30 @@ function initModeler(dataURL, alertURL, modelDir) {
 
 	// when a new document is added to netdata, pass it to models
 	// TODO unify into single model_dispatch function
-	return netDataChannel.subscribe('newpacket', function(newData) {
+	var subscription = netDataChannel.subscribe('newpacket', function(newData) {
 		
 		// TODO collect model alerts by iterating through models object asyncly
-		var modelAlerts = [models.socialbeacon.model(newData)];
-		// save nonempty elements to alerts DB
-		// http://stackoverflow.com/a/5443800/2023432
-		insertAlerts(modelAlerts.filter(Boolean), alertDB);
+    async.forEachOf(models, function(thisModel, modelName, errCallback) {
+      try {
+        var modelAlert = thisModel.model(newData);
+        if (modelAlert !== undefined) {
+          insertAlerts(modelAlert, alertDB);
+        }
+        errCallback();
+      } catch (err) {
+        errCallback(err);
+      }
+    }, function(error) { // individual model error callbacks
+      console.error("Model error: ");
+      console.error(error);
+    });
 	});
+
+  // return info about the subscription
+  return {
+    models: models,
+    dataSubscription: subscription
+  };
 }
 
 function insertAlerts(objarray, db) {

427185a81ca75510b5a2657bedf0ee4d5df0d467
Trevor Alexander <talex@privatdemail.net>
2015-09-29 00:52:14 +0200

    modeler tests-- first two passing

--- backend/tests/unit/modeler_tests.js
+++ backend/tests/unit/modeler_tests.js
@@ -10,9 +10,7 @@ var expect = chai.expect;
 // chai.use(require('sinon-chai'));
 // var sinon = require('sinon');
 var fs = require('fs');
-var modelMaster = require('../../../backend/parser/model_master');
-var mongojs = require('mongojs');
-var mubsub = require('mubsub');
+var modelMaster = require('../../../backend/modeler/model_master');
 
 suite('model init', function() {
   setup(function() {
@@ -23,16 +21,26 @@ suite('model init', function() {
   it('loads as many model functions as are in models dir', function() {
     // not actually the model functions but the subdirs containing the files
     // that...anyway
-    var modelList = fs.readdirSync('../../../backend/modeler/models');
-    
+    var modelList = fs.readdirSync('./backend/modeler/models');
+    var modelInstance = modelMaster.initModeler();
+    expect(modelInstance.models.length === modelList.length);
   });
 
-  it('subscribes to netdata collection', function() {});
+  it('subscribes to netdata collection', function() {
+    var modelInstance = modelMaster.initModeler();
+    expect(modelInstance.dataSubscription);
+  });
 
-  it('connects to alerts db', function() {});
+  it('connects to alerts db', function() {
+    expect(false).to.be.true;
+  });
 
   // TODO generate mock data for these, matching models
-  it('it produces alert objects matching spec', function() {});
+  it('it produces alert objects matching spec', function() {
+    expect(false).to.be.true;
+  });
 
-  it('inserts alerts into alerts db', function() {});
+  it('inserts alerts into alerts db', function() {
+    expect(false).to.be.true;
+  });
 });
